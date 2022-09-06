b5aca2b7ff6eead08c118a11465dfa7e45486596
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-06 16:49:14 +0200

    Added Search Form in Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from "./Logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <div className="horizontal-line" />
+          <small>
+            This project was coded by Katharina Ramer and is {""}
+            <a href="https://github.com/supertramp222/dictionary-project">
+              open-sourced on Github
+            </a>
+            .
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
