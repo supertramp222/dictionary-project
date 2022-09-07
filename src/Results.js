b91b04fa77b2e9102fc3bb81ca34034be8affaf1
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-07 13:02:47 +0200

    Rendered the word meanings

---
+++ src/Results.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Meaning from "./Meaning";
+import "./Results.css";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h1>{props.results.word}</h1>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+              <div className="horizontal-line" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
