31496193831b5625a37d4aae2bb62c2e531c20a2
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-07 11:10:57 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,12 +6,13 @@ export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
+    //documentation: https://ditionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
