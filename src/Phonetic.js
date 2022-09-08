690183b9af534516751fbd64c8fb84995912c9f8
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-08 11:54:08 +0200

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetics">
       <h2>{props.phonetic.text}</h2>
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
     </div>
